7d5fe0cfdd5b3a013c4f29b549944d5910f87217
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-23 19:14:40 +0530

    text editor initial setup and used toolbar from quill

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,6 +1,18 @@
 import { useCallback } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
+import './styles.css';
+
+const TOOLBAR_OPTIONS = [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ font: [] }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["bold", "italic", "underline"],
+    [{ script: "sub" }, { script: "super" }],
+    [{ align: [] }],
+    ["image", "blockquote", "code-block"],
+    ["clean"],
+]
 
 
 const TextEditor = () => {
@@ -11,11 +23,11 @@ const TextEditor = () => {
         wrapper.innerHTML = ''
         const editor = document.createElement('div');
         wrapper.append(editor)
-        new Quill('#container', { theme: 'snow' });
+        new Quill(editor, { theme: 'snow', modules : { toolbar: TOOLBAR_OPTIONS } });
 
     }, [])
   
-    return <div id="container" ref={wrapperRef}></div>
+    return <div className="container" ref={wrapperRef}></div>
 }
 
 export default TextEditor;
